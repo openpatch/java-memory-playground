593a748bb1223806f4b736b9a557c0fcdbbee8f6
Mike Barkmin <mike@barkmin.eu>
2024-08-25 23:41:50 +0200

    adapt vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["@codemirror/state", "@codemirror/view"],
+  },
+  resolve: {
+    alias: {
+      "@codemirror/state": path.resolve(
+        __dirname,
+        "./node_modules/@codemirror/state/dist/index.cjs"
+      ),
+      "@codemirror/lang-yaml": path.resolve(__dirname),
+      "@codemirror/lang-json": path.resolve(
+        __dirname,
+        "./node_modules/@codemirror/lang-json/dist/index.cjs"
+      ),
+    },
+  },
+});
